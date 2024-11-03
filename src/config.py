ddfb2df0e7fb8940939f2e438e60f4f31bbd7d54
Alexey Sokolov <cokol2122@yandex.ru>
2024-11-03 23:33:21 +0300

    add Diplom_3

---
+++ src/config.py
@@ -0,0 +1,4 @@
+BASE_URL = "https://stellarburgers.nomoreparties.site"
+REGISTER_URL = "https://stellarburgers.nomoreparties.site/register"
+RESTORE_URL = "https://stellarburgers.nomoreparties.site/forgot-password"
+BASE_LOGIN = "https://stellarburgers.nomoreparties.site/login"
